1c02a8743139b93b5dc598f113ab364ef342679d
Rithwik Mylavarapu <my.rithwik@berkeley.edu>
2021-03-03 15:49:47 -0800

    idk

--- Hackshop 2/SFSymbolCollection Starter/SFSymbolCollection/SymbolCollection/CollectionVC.swift
+++ Hackshop 2/SFSymbolCollection Starter/SFSymbolCollection/SymbolCollection/CollectionVC.swift
@@ -8,11 +8,45 @@
 import UIKit
 
 class CollectionVC: UIViewController {
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 30
+        layout.minimumInteritemSpacing = 30
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.register(SymbolCollectionCell.self, forCellWithReuseIdentifier: SymbolCollectionCell.reuseIdentifier)
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
 
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds.inset(by: UIEdgeInsets(top: 88, left: 30, bottom: 0, right: 30))
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 }
 
+extension CollectionVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return SymbolProvider.symbols.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let symbol = SymbolProvider.symbols[indexPath.item]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SymbolCollectionCell.reuseIdentifier, for: indexPath) as! SymbolCollectionCell
+        cell.symbol = symbol
+        return cell
+    }
+}
 
+extension CollectionVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 80, height: 100)
+    }
+}
--- Hackshop 2/SFSymbolCollection Starter/SFSymbolCollection/SymbolCollection/SymbolCollectionCell.swift
+++ Hackshop 2/SFSymbolCollection Starter/SFSymbolCollection/SymbolCollection/SymbolCollectionCell.swift
@@ -8,5 +8,57 @@
 import UIKit
 
 class SymbolCollectionCell: UICollectionViewCell {
+    static let reuseIdentifier: String = String(describing: SymbolCollectionCell.self)
     
+    var symbol: SFSymbol? {
+        didSet {
+            imageView.image = symbol?.image
+            titleView.text = symbol?.name
+        }
+    }
+    
+    private let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.tintColor = .white
+        iv.contentMode = .scaleAspectFit
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        
+        return iv
+    }()
+    
+    private let titleView: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        contentView.addSubview(imageView)
+        contentView.addSubview(titleView)
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            titleView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titleView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            titleView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            imageView.widthAnchor.constraint(equalTo: contentView.widthAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 88)
+            
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
